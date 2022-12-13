a74b3198126dfc2a100f0d52c6b2f0e240088dd6
afrojani <sultana.ani2019@gmail.com>
2022-12-12 18:58:27 -0600

    creating my portfolio

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div>
+            my contacts.
+        </div>
+    );
+};
+
+export default Contact;
---
+++ src/components/Skills/Skills.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Skills = () => {
+    return (
+        <div>
+            skills are here.
+        </div>
+    );
+};
+
+export default Skills;
