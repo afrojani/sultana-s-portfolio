5de087d160dea3195696c80b5948f128a69c1ef5
afrojani <sultana.ani2019@gmail.com>
2022-12-12 20:16:27 -0600

    projects section done

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,10 +1,53 @@
 import React from 'react';
+import { projects } from "../../data";
 
 const Projects = () => {
     return (
-        <div>
-            my projects.
-        </div>
+        <section id="projects" className="text-gray-400 bg-gray-900 body-font">
+            <div className="container px-5 py-10 mx-auto text-center lg:px-40">
+                <div className="flex flex-col w-full mb-20">
+                    {/* <CodeIcon className="mx-auto inline-block w-10 mb-4" /> */}
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="mx-auto inline-block w-10 mb-4">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5" />
+                    </svg>
+
+                    <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
+                        Apps I've Built
+                    </h1>
+                    <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+                        Check out some of my recent works. Click the image to go to the live site. I have attached my git links of the projects too.
+                    </p>
+                </div>
+                <div className="flex flex-wrap -m-4">
+                    {projects.map((project) => (
+                        <React.Fragment key={project.imgurl}>
+                            <a
+                                href={project.liveurl}
+                                className="sm:w-1/2 w-100 p-4">
+                                <div className="flex relative">
+                                    <img
+                                        alt="gallery"
+                                        className="absolute inset-0 w-full h-full object-cover object-center"
+                                        src={project.imgurl}
+                                    />
+                                    <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
+                                        <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
+                                            {project.subtitle}
+                                        </h2>
+                                        <h1 className="title-font text-lg font-medium text-white mb-3">
+                                            {project.name}
+                                        </h1>
+                                        <p className="leading-relaxed">{project.description}</p>
+                                    </div>
+                                </div>
+                            </a>
+                            <a href={project.clientlink} className='btn-sm rounded btn-primary my-5'>Git Client side link</a>
+                            <a href={project.serverlink} className='btn-sm rounded btn-primary ml-6 my-5'>Git server side link</a>
+                        </React.Fragment>
+                    ))}
+                </div>
+            </div>
+        </section>
     );
 };
 
---
+++ src/data.js
@@ -0,0 +1,29 @@
+export const projects = [
+    {
+        name: "Bengali Savory",
+        subtitle: "MERN Stack",
+        description: "A cloud kitchen application.The main work of this project was storing data from client side to database.",
+        imgurl: "https://media.istockphoto.com/id/482121467/photo/indian-thali-with-fish.jpg?s=612x612&w=0&k=20&c=VN1wILU7RRC-ey9llvVpB7-YGemIz4LZfv3pO5g8qd4=",
+        liveurl: "https://bengali-savory.web.app/",
+        clientlink: "https://github.com/afrojani/service-review-website-using-MERN-client-side",
+        serverlink: "https://github.com/afrojani/service-review-website-using-MERN-SERVER-side"
+    },
+    {
+        name: "Learner's choice",
+        subtitle: "React and Firebase",
+        description: "An educational app. Developed using Express JS in the backend, and Bootstrap, CSS, and HTML in the frontend.",
+        imgurl: "https://www.hostinger.com/tutorials/wp-content/uploads/sites/2/2021/08/learn-coding-online-for-free.png",
+        liveurl: "https://car-fair-12.web.app/",
+        clientlink: "https://github.com/afrojani/react-app-with-firebase-authentication-CLIENT-side",
+        serverlink: "https://github.com/afrojani/react-app-with-firebase-authentication-SERVER-side"
+    },
+    {
+        name: "CarFair",
+        subtitle: "React and Dashboard",
+        description: "A Second Hand car buying and selling application.Built a dashboard.Admin, Buyer and Seller role for different email login.",
+        imgurl: "https://image.shutterstock.com/image-photo/cars-sale-stock-row-car-260nw-1906135519.jpg",
+        liveurl: "https://car-fair-12.web.app/",
+        clientlink: "https://github.com/afrojani/second-hand-product-website-client-side",
+        serverlink: "https://github.com/afrojani/second-hand-product-website-server-side"
+    },
+];
